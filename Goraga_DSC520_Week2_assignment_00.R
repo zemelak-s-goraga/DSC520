431706c14adf4d148c9a5c03cee4fb4bc9edf3b7
zemelak-s-goraga <143756889+zemelak-s-goraga@users.noreply.github.com>
2023-12-09 23:05:32 -0500

    Add files via upload

---
+++ Goraga_DSC520_Week2_assignment_00.R
@@ -0,0 +1,40 @@
+# DSC 520
+# Week2
+# Assignment: ASSIGNMENT 0
+# Name: Goraga, Zemelak
+# Date: 2023-12-09
+
+
+# Basics
+
+# Add 8 and 5
+8 + 5
+# Subtract 6 from 22
+22 - 6
+# Multiply 6 by 7
+6 * 7
+# Add 4 to 6 and divide the result by 2
+(4 + 6) / 2
+# Compute 5 modulo 2
+5 %% 2
+# Assign the value 82 to the variable x and print x
+x <- 82
+print(x)
+# Assign the value 41 to the variable y and print y
+y <- 41
+print(y)
+# Assign the output of x + y to the variable z and print z
+z <- x + y
+print(z)
+# Assign the string value "DSC520" to the variable class_name and print class_name
+class_name <- "DSC520"
+print(class_name)
+# Assign the string value of TRUE to the variable is_good and print is_good
+is_good <- TRUE
+print(is_good)
+# Check the class of the variable is_good using the `class()` function
+print(class(is_good))
+# Check the class of the variable z using the `class()` function
+print(class(z))
+# Check the class of the variable class_name using the class() function
+print(class(class_name))
